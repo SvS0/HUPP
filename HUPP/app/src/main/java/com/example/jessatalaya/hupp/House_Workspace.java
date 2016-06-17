cca4871adffdddf83fa704500ca5b8f29f8a9a0f
suso01 <susocorleone@gmail.com>
2016-06-17 16:19:27 +0200

    first

---
+++ HUPP/app/src/main/java/com/example/jessatalaya/hupp/House_Workspace.java
@@ -0,0 +1,15 @@
+package com.example.jessatalaya.hupp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class House_Workspace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house__workspace);
+
+        
+    }
+}
